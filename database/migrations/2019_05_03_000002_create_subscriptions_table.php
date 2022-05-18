84ba00ed178b1e011aed27565c81df26237f4621
ashishparas <ashish@parastechnologies.vcom>
2022-05-18 16:15:29 +0530

    integrate subscription functionality to the app

--- database/migrations/2019_05_03_000002_create_subscriptions_table.php
+++ database/migrations/2019_05_03_000002_create_subscriptions_table.php
@@ -18,11 +18,14 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->string('name');
             $table->string('stripe_id')->unique();
+            $table->string('subscription_id')->nullable();
+            $table->string('subscription_item_id')->nullable();
             $table->string('stripe_status');
             $table->string('stripe_price')->nullable();
             $table->integer('quantity')->nullable();
             $table->timestamp('trial_ends_at')->nullable();
             $table->timestamp('ends_at')->nullable();
+            $table->text('body')->nullable();
             $table->timestamps();
 
             $table->index(['user_id', 'stripe_status']);
