26f05d3fb8dc476b7bc62b19c8257503000211eb
ashishparas <ashish@parastechnologies.vcom>
2022-05-20 13:08:55 +0530

    debug issue

--- app/Http/Middleware/AdvancedProducts.php
+++ app/Http/Middleware/AdvancedProducts.php
@@ -32,21 +32,41 @@ class AdvancedProducts extends ApiController
 ];
         }elseif($request->type === '2'){
            
-            $rules = ['name'=> 'required','image'=>'required','category_id' =>'required',
-
+            $rules = [
+                'name'=> 'required',
+                'image'=>'required',
+                'category_id' =>'required',
                 'subcategory_id' => 'required|exists:subcategories,id',
-                'description' => 'required', 'price' => 'required', 'discount' => '',
-                'brand' => 'required', 'color' =>'required', 'quantity' => 'required',
-                'weight' =>'required', 'condition'=>'required', 'dimensions' =>'',
-                'available_for_sale' => 'required|in:1,2','constomer_contact'=> 'required|in:1,2',
-                'inventory_track' => 'required|in:1,2','product_offer' => '','ships_from'=>'required', 'shipping_type' => 'required', 'meta_description' => '',
-                'meta_tags' => '', 'meta_keywords' => '', 'title' => '', 'variants' => 'required',
-                'state'=> '','tags' =>'','advertisement' =>'', 'selling_fee' =>'required', 
-                'amount' => 'required','type'=>'required','store_id' =>'required|exists:stores,id'
-    ];
+                'description' => 'required',
+                'price' => 'required',
+                'discount' => '',
+                'brand' => 'required',
+                'color' =>'required', 
+                'quantity' => 'required',
+                'weight' =>'required', 
+                'condition'=>'required', 
+                'dimensions' =>'',
+                'available_for_sale' => 'required|in:1,2',
+                'constomer_contact'=> 'required|in:1,2',
+                'inventory_track' => 'required|in:1,2',
+                'product_offer' => '',
+                'ships_from'=>'required', 
+                'shipping_type' => 'required', 
+                'meta_description' => '',
+                'meta_tags' => '', 
+                'meta_keywords' => '', 
+                'title' => '', 
+                'variants' => 'required',
+                'state'=> '',
+                'tags' =>'',
+                'advertisement' =>'', 
+                'selling_fee' =>'required', 
+                'amount' => 'required',
+                'type'=>'required',
+                'store_id' =>'required|exists:stores,id'];
         }
 
-        $validateAttributes = parent::validateAttributes($request, 'POST', $rules, array_keys($rules), false);
+        $validateAttributes = parent::validateAttributes($request,'POST', $rules, array_keys($rules), false);
         if($validateAttributes):
             return $validateAttributes;
         endif;
