35bf434e1aa9af9a22c293a1972d133339f14f0a
ashishparas <ashish@parastechnologies.vcom>
2022-05-26 13:14:45 +0530

    product by id api

--- app/Http/Controllers/API/ClientController.php
+++ app/Http/Controllers/API/ClientController.php
@@ -234,7 +234,7 @@ class ClientController extends ApiController
         try{
 
             $input = $request->all();
-            $product = Product::FindOrfail($input['product_id'])->first();
+            $product = Product::where('id',$input['product_id'])->first();
           
             $product['rating'] = number_format($product->Rating()->avg('rating'),1);
             $product['RatingCount'] = $product->Rating()->count('product_id');
