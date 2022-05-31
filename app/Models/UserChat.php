471d54a354f35fbaab7aaf2b5773046edc0dce00
ashishparas <ashish@parastechnologies.vcom>
2022-05-31 19:32:22 +0530

    added offer to user chat

---
+++ app/Models/UserChat.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserChat extends Model
+{
+    use HasFactory;
+
+    protected $table = "user_chat";
+
+
+    protected $primaryKey = "id";
+
+
+    protected $fillable = ['roomID','source_user_id','target_user_id','message','MessageType','status','modified_on','created_on'];
+
+    protected $appends = ['Offer'];
+
+    public function getOfferAttribute(){
+        return Offer::where('id', intval($this->message))->first();
+    }
+}
