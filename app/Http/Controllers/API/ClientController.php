d5d67cc1193b3f4060b4f6382ff911cc857bb068
ashishparas <ashish@parastechnologies.vcom>
2022-06-01 12:51:35 +0530

    added social media params in client view profile

--- app/Http/Controllers/API/ClientController.php
+++ app/Http/Controllers/API/ClientController.php
@@ -1206,7 +1206,7 @@ class ClientController extends ApiController
             // }
         
             if(!empty($RecentChat)) {
-                $offer = Offer::where('vendor_id',$request->reciever_id)->orWhere('vendor_id', Auth::id())->take(1)->orderBy('created_at',"DESC")->first();
+                $offer = Offer::where('vendor_id',$request->reciever_id)->orWhere('vendor_id', Auth::id())->take(1)->orderBy('created_at',"DESC")->with('Product')->first();
      return $response=array("status"=>true,"code"=>200,"message"=>"View Messages successfully!","offer" => $offer,"data" =>$RecentChat,"blockStatus" => 0,"BlockByID" =>0);  
             // return parent::success(['message' => 'View Messages successfully!','data' => $RecentChat,"blockStatus" => $blockStatus,"BlockByID" =>$block_by_id]);
             }else {
