39f8fcdb44bb01181450d84bcfc91a134d75010b
ashishparas <ashish@parastechnologies.vcom>
2022-06-07 10:43:44 +0530

    change in response filter api

--- app/Models/RecentProducts.php
+++ app/Models/RecentProducts.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class RecentProducts extends Model
 {
@@ -20,12 +21,20 @@ protected $primaryKey = "id";
 protected $fillable = ['user_id','product_id','updated_at'];
 
 
+protected $appends = ['FavouriteId'];
 
 public function Product(){
     return $this->belongsTo(Product::class)->select('id','name','image','price','brand');
 }
 
-
+public function getFavouriteIdAttribute(){
+  
+    $favourite = Favourite::where('product_id', $this->id)
+     ->where('by', Auth::id())
+     ->first();
+     $ft = (!$favourite)?null:$favourite->id; 
+     return (string)$ft;
+ }
 
 
 }
