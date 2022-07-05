19aab01de10d9f42728adee3764c45e27e53833b
ashishparas <ashish@parastechnologies.vcom>
2022-07-05 18:10:16 +0530

    change in view order by id api

--- resources/views/admin/brand/brand-list.blade.php
+++ resources/views/admin/brand/brand-list.blade.php
@@ -59,8 +59,7 @@
     </div>
 	</div>
 
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-
+  
   <script type="text/javascript">
     $(function () {
     
