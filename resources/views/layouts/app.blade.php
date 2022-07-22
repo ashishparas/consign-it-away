2f8723de49f4263b509c233d8e8eb8c5710697d6
ashishparas <ashish@parastechnologies.vcom>
2022-07-22 13:11:08 +0530

    add product

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -334,7 +334,7 @@
 </script>
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"> </script>
 <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"> </script>
-{{-- <script src="{{asset('public/js/custom.js')}}"></script> --}}
+<script src="{{asset('public/js/custom.js')}}"></script>
 <script>
   $(document).ready(function() {
       $('#example').DataTable();
